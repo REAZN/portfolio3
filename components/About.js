3dd6a5714676a0fec5172ca7ff85d61218e7c74c
reazn <github@reazn.me>
2022-06-15 07:24:00 +0100

    working version of fullpage

---
+++ components/About.js
@@ -0,0 +1,11 @@
+import styles from "styles/components/About.module.scss";
+// import { motion, AnimatePresence } from "framer-motion";
+// import config from "config";
+
+export default function About() {
+    return (
+        <section>
+            About Page
+        </section>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import 'styles/style.scss'
+import "styles/overide.scss"
+import { motion, AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps, router }) {
+
+  return (
+      // <AnimatePresence exitBeforeEnter initial={false}>
+      //     <motion.div
+      //         key={router.route}
+      //         initial={{y: "-100%"}}
+      //         animate={{y: "-0"}}
+      //         transition={{ duration: 0.7, ease: "easeInOut" }}
+      //     >
+      //     </motion.div>
+      // </AnimatePresence>
+        <Component {...pageProps} />
+  )
 }
 
-export default MyApp
