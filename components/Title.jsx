38dae6756c4f84f8eb6557d4fe3616652443cfea
reazn <github@reazn.me>
2022-06-21 17:35:40 +0100

    title changes

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,10 +1,11 @@
 import styles from "styles/components/title.module.scss";
 
 
-export default function Title({title = "Title"}) {
+export default function Title({title = "Title", center = false}) {
     return (
         <div className={styles.title}>
             <h1>{title}</h1>
+            <div className={styles.line} style={center ? {margin: "auto"} : {}}/>
         </div>
     )
 }
