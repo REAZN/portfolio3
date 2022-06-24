e13bdc663c28667463b9221e3bc57ecaca00435d
reazn <github@reazn.me>
2022-06-24 03:11:40 +0100

    nav animation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,15 +2,14 @@ import styles from "styles/components/nav.module.scss";
 import { useState, useEffect } from "react";
 import {useRouter} from "next/router";
 
-
 export default function Nav({pages}) {
-    const router = useRouter()
+    const router = useRouter();
     console.log(router.asPath)
     let [path, setPath] = useState(router.asPath.replace("/#", ""));
 
     useEffect(() => {
         window.addEventListener("hashchange", () => {
-            setPath(window.location.hash.replace("#", ""))
+            setPath(window.location.hash.replace("#", ""));
         })
     },[]);
 
@@ -18,7 +17,7 @@ export default function Nav({pages}) {
         <header className={styles.navContainer}>
             <nav className={styles.nav}>
                 <ul className={styles.navLinks} id={"menu"}>
-                    {pages.map((link, index) => (
+                    {pages.map(link => (
                         <li key={link} className={`${styles.link} ${path === link ? styles.active : ""}`}>
                             <a href={`#${link}`}>{link}</a>
                         </li>
